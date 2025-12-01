4d24df816569f91fab339b826417c2f49796a6c3
gravek <gravek@live.ru>
2025-12-01 21:38:44 +0300

    чище

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './App.jsx';
+import App from './App.jsx';   // ← импортируем как App
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
