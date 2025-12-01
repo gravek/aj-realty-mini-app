628776c55d889a63849518439d55243db08cc17f
gravek <gravek@live.ru>
2025-12-02 00:44:07 +0300

    Revert "чище" 905b3f1 full offline mode + photos This reverts commit 4d24df816569f91fab339b826417c2f49796a6c3.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';   // ← импортируем как App
+import Layout from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Layout />
     </BrowserRouter>
   </React.StrictMode>
 );
