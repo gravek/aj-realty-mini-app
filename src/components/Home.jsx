1ece03b547a29d31378060f02b5652752677aa1c
gravek <gravek@live.ru>
2026-01-20 19:43:03 +0300

    open_home logging test 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,12 +40,12 @@ export default function Home() {
     .slice(0, 6);
 
   useEffect(() => {
-    console.log('!!! logEvent open_home вызывается !!!');
-    logEvent('open_home', { test: 'from home' });
-    
+    // console.log('!!! logEvent open_home вызывается !!!');
+    // logEvent('open_home', { test: 'from home' });
+
     const key = 'logged_open_home';
 
-    if (localStorage.getItem(key)) return; // если уже логировали
+    // if (localStorage.getItem(key)) return; // если уже логировали
 
     logEvent('open_home', {});
 
